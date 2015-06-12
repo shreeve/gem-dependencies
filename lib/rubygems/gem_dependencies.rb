6c1aeacc8521161a32316950f72648297156ca18
Steve Shreeve <steve.shreeve@gmail.com>
2015-06-12 04:12:13 -0400

    a plugin, not an executable script

--- lib/rubygems/gem_dependencies.rb
+++ lib/rubygems/gem_dependencies.rb
@@ -1,5 +1,3 @@
-#!/usr/bin/env ruby
-
 # =============================================================================
 # gem_dependencies.rb: Simplifies installing binary gems on runtime systems
 #
